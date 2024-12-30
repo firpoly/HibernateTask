fc45262e4a3d2431000d93e715807e3d888f6abb
Firsova Polina <firpoly@gmail.com>
2024-12-30 22:11:11 +0300

    домашняя работа

--- src/main/java/com/example/HibernateTask/controller/HibernateTaskController.java
+++ src/main/java/com/example/HibernateTask/controller/HibernateTaskController.java
@@ -3,11 +3,16 @@ package com.example.HibernateTask.controller;
 import com.example.HibernateTask.model.Person;
 import com.example.HibernateTask.model.PersonInfo;
 import com.example.HibernateTask.service.HibernateTaskService;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.IOException;
 import java.util.List;
 
 @RestController
@@ -40,4 +45,5 @@ public class HibernateTaskController {
                             @RequestParam("age") int age) {
         return service.setPerson(name, surname, age);
     }
+
 }
---
+++ src/main/java/com/example/HibernateTask/controller/SecurityConfig.java
@@ -0,0 +1,41 @@
+package com.example.HibernateTask.controller;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+public class SecurityConfig {
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/persons/by-city").permitAll()
+                        .requestMatchers("/persons/by-age","/persons/by-fi","/persons/setPerson")
+                        .hasAuthority("User")
+                );
+
+        http.formLogin(withDefaults());
+        return http.build();
+
+    }
+    @Bean
+    public InMemoryUserDetailsManager userDetailsService() {
+        UserDetails user = User.withUsername("user")
+                .password(passwordEncoder().encode("userPass"))
+                .roles("USER")
+                .build();
+        return new InMemoryUserDetailsManager(user);
+    }
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
